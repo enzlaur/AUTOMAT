cec6d66a510cc7ac6210c935ff6e0dece892f5e7
laurenz <laurenztino@yahoo.com>
2016-09-27 14:41:10 +0800

    [FIRST PUSH]
    OYEA

---
+++ src/HW1/Converter.java
@@ -0,0 +1,17 @@
+package HW1;
+
+/**
+ * Created by laurenztolentino on 09/26/2016.
+ */
+public class Converter
+{
+	public static void main(String[] args)
+	{
+		println("Hello");
+	}
+
+	public static void println(Object output)
+	{
+		System.out.println(output.toString() + "");
+	}
+}
